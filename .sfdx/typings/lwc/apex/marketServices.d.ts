6f7930bc46fdeef3278e224ff0b1b57c440c6348
Jeanne Pauline Pua <jeanne.pauline.b.pua@accenture.com>
2019-05-15 10:43:55 +0800

    Added Reservation Manager Page components

---
+++ .sfdx/typings/lwc/apex/marketServices.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/marketServices.getRelatedSpaces" {
+  export default function getRelatedSpaces(param: {recordId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/marketServices.getMarketsByState" {
+  export default function getMarketsByState(param: {state: any}): Promise<any>;
+}
